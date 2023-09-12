529ae6c6b6ab3ea32a7b681ca8c784b11c4f79ac
iamyangyiok <102146648+iamyangyiok@users.noreply.github.com>
2023-09-12 12:50:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,6 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
